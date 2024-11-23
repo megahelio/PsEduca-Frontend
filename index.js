3020c89c3c3055a940f50a5da88fe5e62114fa0f
Jose Campo Rivas <josecampor31@gmail.com>
2024-11-23 23:17:51 +0100

    añadir interfaz gráfica de la página "Quienes somos"
    Falta integrarlo con la API, solo muestra información estática

---
+++ index.js
@@ -0,0 +1,129 @@
+import { getPageLanguage, setPageLanguage } from "../../lang/i18n.js";
+
+sessionStorage.setItem('token', 'token');
+sessionStorage.setItem('ROL', 'ADMIN_GLOBAL');
+
+const $ = (elem) => document.querySelector(elem);
+const $$ = (elem) => document.querySelectorAll(elem);
+
+const $listSections = $('.list-sections');
+const $logInLink = $('#log-in-link');
+
+const ROLS = ['ADMIN_GLOBAL','GESTOR_CATALOGO','USUARIO_PYP'];
+if(sessionStorage.getItem('token') && sessionStorage.getItem('ROL')){
+    const ROL = sessionStorage.getItem('ROL');
+    if(!ROLS.includes(ROL)){
+        sessionStorage.removeItem('token');
+        sessionStorage.removeItem('ROL');
+        location.reload();
+    }
+
+    const intranet_section = `
+        <li class="list-item-with-children" id="list-item-with-children">
+            <a href="javascript:void(0)" class="list-item-with-image">
+                <span data-i18n="header.navbar.intranet">Intranet</span>
+                <img src="./images/arrow_right_icon.svg" width="15px" class="rotate-90-deg inverted"/>
+            </a>
+            <ul class="sublist" id="sublist">
+                ${ROL === 'ADMIN_GLOBAL' ? '<li><a href="#" data-i18n="header.navbar.userManagement">Gestión usuarios</a></li>' : ''}
+                ${ROL === 'ADMIN_GLOBAL' ? '<li><a href="#" data-i18n="header.navbar.membersManagement">Gestión miembros</a></li>' : ''}
+                ${ROL === 'ADMIN_GLOBAL' || ROL === 'GESTOR_CATALOGO' ? '<li><a href="#" data-i18n="header.navbar.catalogManagement">Gestión catálogo</a></li>' : ''}
+                ${ROL === 'ADMIN_GLOBAL' ? '<li><a href="#" data-i18n="header.navbar.formationManagement">Gestión formación</a></li>' : ''}
+                ${ROL === 'ADMIN_GLOBAL' ? '<li><a href="#" data-i18n="header.navbar.divulgationManagement">Gestión divulgación</a></li>' : ''}
+                ${ROL === 'ADMIN_GLOBAL' || ROL === 'USUARIO_PYP' ? '<li><a href="#" data-i18n="header.navbar.pypManagement">Gestión PyP</a></li>' : ''}
+            </ul>
+        </li>`;
+    $listSections.innerHTML += intranet_section;
+
+    $logInLink.querySelector('span').textContent = 'Cerrar sesión';
+    $logInLink.querySelector('span').dataset.i18n = 'header.logOut';
+
+    $logInLink.addEventListener('click', (e) => {
+        e.preventDefault();
+        sessionStorage.removeItem('token');
+        sessionStorage.removeItem('ROL');
+        location.reload();
+    });
+}
+
+setPageLanguage();
+
+$('#button-menu').addEventListener('click', (e) => {
+    $listSections.classList.toggle('active');
+    e.stopPropagation();
+});
+
+document.addEventListener('matchMedia', () => {
+    if (window.matchMedia('(max-width: 1050px)').matches) {
+        $listSections.classList.remove('active');
+    }
+});
+
+const $buttonPreview = $('#button-preview-uvigo-footer');
+const $iconButtonPreview = $('#footer-icon-expanded-info')
+const $textButtonPreview = $('#footer-text-button-preview');
+const $expandedInfoUvigo = $('#footer-expanded-uvigo-info');
+
+$buttonPreview.addEventListener('click', () => {
+    $expandedInfoUvigo.classList.toggle('active');
+
+    if ($expandedInfoUvigo.classList.contains('active')) {
+        $textButtonPreview.textContent = 'Ver menos';
+        $iconButtonPreview.style.transform = 'rotate(90deg)';
+    }else{
+        $textButtonPreview.textContent = 'Ver más';
+        $iconButtonPreview.style.transform = 'rotate(0deg)';
+    }
+});
+
+const $$listItemWithSubmenu = $$('.list-item-with-children');
+const $$imgArrow = $$('.list-item-with-children img');
+
+
+$$listItemWithSubmenu.forEach(el => {
+    el.addEventListener('click', () => {
+        if(window.matchMedia('(max-width: 1050px)').matches){
+            el.querySelector('.sublist').classList.toggle('active');
+            const $imgEl = el.querySelector('img')
+            $imgEl.style.transform = $imgEl.style.transform === 'rotate(-90deg)' ? '' : 'rotate(-90deg)';
+        }
+    });
+})
+    
+
+window.addEventListener('resize', () => {
+    if (window.matchMedia('(min-width: 1050px)').matches) {
+        $$('.sublist').forEach((el) => el.classList.remove('active'))
+        $$('.list-item-with-children img').forEach(img => img.style.transform = '')
+        $listSections.classList.remove('active');
+    }
+});
+
+document.addEventListener('click', (e) => {
+    if (window.matchMedia('(max-width: 1050px)').matches) {
+        if (e.target !== $listSections && !$listSections.contains(e.target) && $listSections.classList.contains('active')) {
+            $listSections.classList.toggle('active');
+        }  
+    }
+});
+
+const $$languageButtons = $$('.language-selector-button');
+
+$$languageButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        $$languageButtons.forEach((button) => button.classList.remove('language-selected'));
+        button.classList.add('language-selected');
+    });
+});
+
+const $$buttonsLanguage = $$('.language-selector-button');
+
+$$buttonsLanguage.forEach((button) => {
+    button.addEventListener('click', () => {
+        setPageLanguage(button.getAttribute('data-language'));
+    });
+
+    if (button.getAttribute('data-language') === getPageLanguage()) {
+        button.classList.add('language-selected');
+    }
+});
