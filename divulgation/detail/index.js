e6788d2c4fab9190ba7fd3915224ad45a995feea
Jose Campo Rivas <josecampor31@gmail.com>
2025-01-06 19:08:38 +0100

    Paginas divulgacion (vista publica)

---
+++ divulgation/detail/index.js
@@ -0,0 +1,162 @@
+import { getPageLanguage, setPageLanguage } from "../../lang/i18n.js";
+import { createMember, deleteMember, getMemberById, updateMember } from "../../queries/members.js";
+import { validateMember } from "../../validators/member.js";
+import config from "../../config.js";
+import { getDivulgationById } from "../../queries/divulgations.js";
+
+const $ = (elem) => document.querySelector(elem);
+const $$ = (elem) => document.querySelectorAll(elem);
+
+const $listSections = $('.list-sections');
+const $logInLink = $('#log-in-link');
+
+const ROLS = ['ADMIN_GLOBAL','GESTOR_CATALOGO','USUARIO_PYP'];
+const SERVER_URL = config.SERVER_URL;
+if(sessionStorage.getItem('token') && sessionStorage.getItem('ROL')){
+    const ROL = sessionStorage.getItem('ROL');
+    if(!ROLS.includes(ROL)){
+        sessionStorage.removeItem('token');
+        sessionStorage.removeItem('ROL');
+        location.reload();
+    }
+
+    const intranet_section = `
+        <li class="list-item-with-children" id="list-item-with-children">
+            <a href="javascript:void(0)" class="list-item-with-image">
+                <span data-i18n="header.navbar.intranet">Intranet</span>
+                <img src="../../images/arrow_right_icon.svg" width="15px" class="rotate-90-deg inverted"/>
+            </a>
+            <ul class="sublist" id="sublist">
+                ${ROL === 'ADMIN_GLOBAL' ? '<li><a href="../../admin/users/index.html" data-i18n="header.navbar.userManagement">Gestión usuarios</a></li>' : ''}
+                ${ROL === 'ADMIN_GLOBAL' ? '<li><a href="../../admin/members/index.html" data-i18n="header.navbar.membersManagement">Gestión miembros</a></li>' : ''}
+                ${ROL === 'ADMIN_GLOBAL' || ROL === 'GESTOR_CATALOGO' ? '<li><a href="#" data-i18n="header.navbar.catalogManagement">Gestión catálogo</a></li>' : ''}
+                ${ROL === 'ADMIN_GLOBAL' ? '<li><a href="../../admin/formation/index.html" data-i18n="header.navbar.formationManagement">Gestión formación</a></li>' : ''}
+                ${ROL === 'ADMIN_GLOBAL' ? '<li><a href="../../admin/divulgation/index.html" data-i18n="header.navbar.divulgationManagement">Gestión divulgación</a></li>' : ''}
+                ${ROL === 'ADMIN_GLOBAL' || ROL === 'USUARIO_PYP' ? '<li><a href="#" data-i18n="header.navbar.pypManagement">Gestión PyP</a></li>' : ''}
+            </ul>
+        </li>`;
+    $listSections.innerHTML += intranet_section;
+
+    $logInLink.querySelector('span').textContent = 'Cerrar sesión';
+    $logInLink.querySelector('span').dataset.i18n = 'header.logOut';
+
+    $logInLink.addEventListener('click', (e) => {
+        e.preventDefault();
+        sessionStorage.removeItem('token');
+        sessionStorage.removeItem('ROL');
+        location.reload();
+    });
+}
+
+setPageLanguage();
+
+const path = window.location.search;
+
+const REGEX_PATH = /\?id=[0-9]+$/;
+
+if (!REGEX_PATH.test(path)) {
+    window.location.href = '../../index.html'
+}
+
+async function loadDivulgation(){
+    const id = path.split('=')[1];
+
+    if(id !== '0'){
+        const divulgation = await getDivulgationById(id)
+    
+        const $errorMessage = $('#error-message-all')
+        if (divulgation.error) {
+            $errorMessage.textContent = divulgation.error;
+            $errorMessage.classList.add('active');
+            return;
+        }
+
+        $('#divulgation-detail-title').textContent = divulgation.title;
+        //$('#divulgation-detail-image').src = divulgation.image;
+        $('#divulgation-detail-content').innerHTML = divulgation.content;
+    }else{
+        $('#error-message-all').textContent = 'Error'
+        $('#error-message-all').classList.add('active');
+    }
+}
+
+loadDivulgation();
+
+$('#button-menu').addEventListener('click', (e) => {
+    $listSections.classList.toggle('active');
+    e.stopPropagation();
+});
+
+document.addEventListener('matchMedia', () => {
+    if (window.matchMedia('(max-width: 1050px)').matches) {
+        $listSections.classList.remove('active');
+    }
+});
+
+const $buttonPreview = $('#button-preview-uvigo-footer');
+const $iconButtonPreview = $('#footer-icon-expanded-info')
+const $textButtonPreview = $('#footer-text-button-preview');
+const $expandedInfoUvigo = $('#footer-expanded-uvigo-info');
+
+$buttonPreview.addEventListener('click', () => {
+    $expandedInfoUvigo.classList.toggle('active');
+
+    if ($expandedInfoUvigo.classList.contains('active')) {
+        $textButtonPreview.textContent = 'Ver menos';
+        $iconButtonPreview.style.transform = 'rotate(90deg)';
+    }else{
+        $textButtonPreview.textContent = 'Ver más';
+        $iconButtonPreview.style.transform = 'rotate(0deg)';
+    }
+});
+
+const $$listItemWithSubmenu = $$('.list-item-with-children');
+const $$imgArrow = $$('.list-item-with-children img');
+
+
+$$listItemWithSubmenu.forEach(el => {
+    el.addEventListener('click', () => {
+        if(window.matchMedia('(max-width: 1050px)').matches){
+            el.querySelector('.sublist').classList.toggle('active');
+            const $imgEl = el.querySelector('img')
+            $imgEl.style.transform = $imgEl.style.transform === 'rotate(-90deg)' ? '' : 'rotate(-90deg)';
+        }
+    });
+})
+
+window.addEventListener('resize', () => {
+    if (window.matchMedia('(min-width: 1050px)').matches) {
+        $$('.sublist').forEach((el) => el.classList.remove('active'))
+        $$('.list-item-with-children img').forEach(img => img.style.transform = '')
+        $listSections.classList.remove('active');
+    }
+});
+
+document.addEventListener('click', (e) => {
+    if (window.matchMedia('(max-width: 1050px)').matches) {
+        if (e.target !== $listSections && !$listSections.contains(e.target) && $listSections.classList.contains('active')) {
+            $listSections.classList.toggle('active');
+        }  
+    }
+});
+
+const $$languageButtons = $$('.language-selector-button');
+
+$$languageButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        $$languageButtons.forEach((button) => button.classList.remove('language-selected'));
+        button.classList.add('language-selected');
+    });
+});
+
+const $$buttonsLanguage = $$('.language-selector-button');
+
+$$buttonsLanguage.forEach((button) => {
+    button.addEventListener('click', () => {
+        setPageLanguage(button.getAttribute('data-language'));
+    });
+
+    if (button.getAttribute('data-language') === getPageLanguage()) {
+        button.classList.add('language-selected');
+    }
+});
